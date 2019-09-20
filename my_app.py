605a92efc0b005d14e765dcba81be82c69c1a5e4
yaiir-a <42359972+yaiir-a@users.noreply.github.com>
2019-09-20 19:52:30 +0100

    lol

--- my_app.py
+++ my_app.py
@@ -1,46 +1,46 @@
 from flask import Flask, jsonify, request, make_response
 import requests as r
 from collections import Counter
-# import os
-#
-# try:
-#     from passwords import BEARER
-# except:
-#     BEARER = os.environ['BEARER']
-#
-# class Airtable(object):
-#     def __init__(self):
-#         self.headers = {'Authorization': BEARER,
-#                        'Content-Type': 'application/json'}
-#         self.base_url = "https://api.airtable.com/v0/apphBP3YhZKaBIYti/donuts"
-#
-#     def get_all(self):
-#         params = {'view':'sorted'}
-#         self.resp = r.get(self.base_url, headers=self.headers, params=params)
-#         return self.resp.json()
-#
-#     def create_entry(self, donut):
-#         payload = {
-#             "records":[{'fields':{'donut':donut}}]
-#         }
-#         self.resp = r.post(self.base_url, headers=self.headers, json=payload)
-#         return self.resp.json()
-#
-#     def donuts(self):
-#          entries = self.get_all()
-#          names = [entry['fields']['donut'] for entry in entries['records']]
-#          return names
-#
-#     def latest(self):
-#          names = self.donuts()
-#          return names[0]
-#
-#     def hall_of_shame(self):
-#          names = self.donuts()
-#          return Counter(names).most_common()
-#
-#
-# a = Airtable()
+import os
+
+try:
+    from passwords import BEARER
+except:
+    BEARER = os.environ['BEARER']
+
+class Airtable(object):
+    def __init__(self):
+        self.headers = {'Authorization': BEARER,
+                       'Content-Type': 'application/json'}
+        self.base_url = "https://api.airtable.com/v0/apphBP3YhZKaBIYti/donuts"
+
+    def get_all(self):
+        params = {'view':'sorted'}
+        self.resp = r.get(self.base_url, headers=self.headers, params=params)
+        return self.resp.json()
+
+    def create_entry(self, donut):
+        payload = {
+            "records":[{'fields':{'donut':donut}}]
+        }
+        self.resp = r.post(self.base_url, headers=self.headers, json=payload)
+        return self.resp.json()
+
+    def donuts(self):
+         entries = self.get_all()
+         names = [entry['fields']['donut'] for entry in entries['records']]
+         return names
+
+    def latest(self):
+         names = self.donuts()
+         return names[0]
+
+    def hall_of_shame(self):
+         names = self.donuts()
+         return Counter(names).most_common()
+
+
+a = Airtable()
 
 
 app = Flask(__name__)
