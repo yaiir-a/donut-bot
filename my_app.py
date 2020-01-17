9d1be8cb0d237318f5294de032d7fb2fe5a62bf1
yaiir-a <42359972+yaiir-a@users.noreply.github.com>
2020-01-17 00:20:47 +0000

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -114,7 +114,7 @@ def donut():
     elif bringer:
         if bringer.group(0) != user_id:
             out = 'ok different person reporting'
-            out = f'{bringer.group(0)}, {user_id}, {text}, {user_name}'
+            out = f'{bringer.group(0)}, {user_id}, {text}, {user_name}, {request.form["user_id"]}'
         else:
             out = 'You rascal. You need someone else to vouch that you brought donuts.'
     else:
